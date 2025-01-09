6d2e6de3c08831dc8a9fbb8071b8619d1cd6f23e
mtfishman <mfishman@flatironinstitute.org>
2025-01-09 16:25:50 -0500

    Format

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,7 @@
 using MapBroadcast: MapBroadcast
 using Documenter: Documenter, DocMeta, deploydocs, makedocs
 
-DocMeta.setdocmeta!(
-  MapBroadcast, :DocTestSetup, :(using MapBroadcast); recursive=true
-)
+DocMeta.setdocmeta!(MapBroadcast, :DocTestSetup, :(using MapBroadcast); recursive=true)
 
 include("make_index.jl")
 
@@ -12,13 +10,9 @@ makedocs(;
   authors="ITensor developers <support@itensor.org> and contributors",
   sitename="MapBroadcast.jl",
   format=Documenter.HTML(;
-    canonical="https://ITensor.github.io/MapBroadcast.jl",
-    edit_link="main",
-    assets=String[],
+    canonical="https://ITensor.github.io/MapBroadcast.jl", edit_link="main", assets=String[]
   ),
   pages=["Home" => "index.md"],
 )
 
-deploydocs(;
-  repo="github.com/ITensor/MapBroadcast.jl", devbranch="main", push_preview=true
-)
+deploydocs(; repo="github.com/ITensor/MapBroadcast.jl", devbranch="main", push_preview=true)
